618a2ac62bb19349ce6b88963814d2598f1366b1
Enio Nascimento <enio.analista@gmail.com>
2024-11-07 13:33:09 -0300

    primeiro commit

---
+++ src/funcoes.js
@@ -0,0 +1,9 @@
+"use strict";
+function mutiplicar(number1, number2) {
+    return number1 * number2;
+}
+console.log(mutiplicar(3, 4));
+function ola(nome) {
+    return `Olá!, ${nome}.`;
+}
+console.log(ola('Jose'));
---
+++ src/funcoes.ts
@@ -0,0 +1,13 @@
+
+
+ function mutiplicar(number1: number,number2: number){
+    return number1 * number2;
+}
+
+console.log(mutiplicar(3 , 4))
+
+
+function ola(nome: string){
+    return `Olá!, ${nome}.`
+}
+console.log(ola('Jose'))
